b1bfac4e923f531a69bf8b8392d6d01886649a95
eka0907@mail.ru <eka0907@mail.ru>
2022-02-21 01:54:31 +0300

    выравнивание отправленных - направо

---
+++ Homework10.TelegramBot.Ui/Converter/MessageDirectionToAlignment.cs
@@ -0,0 +1,28 @@
+﻿using Homework10.TelegramBot.Ui.Models;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Homework10.TelegramBot.Ui.Converter;
+
+public class MessageDirectionToAlignment : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var direction = (MessageDirection)value;
+        if (direction == MessageDirection.Send)
+        {
+            return HorizontalAlignment.Right;
+        }
+        else
+        {
+            return HorizontalAlignment.Left;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
